7ed68886cfdb44c21c5ca106fbae4d7bed4043a1
adityaverma321 <59736486+adityaverma321@users.noreply.github.com>
2023-01-04 19:04:13 +0530

    Initial commit

---
+++ pro12.py
@@ -0,0 +1,16 @@
+#program 12.Reading images the through video frame from camera / video file
+import cv2
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Camera not open")
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    img = cv2.flip(frame,1)
+    cv2.imshow("Camera",img)
+    if cv2.waitKey(25) & 0xFF == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
